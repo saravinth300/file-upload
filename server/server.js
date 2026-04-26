40937e2b4290aa62c8122d53679292af47b79b80
saravinth300 <162457737+saravinth300@users.noreply.github.com>
2026-04-26 21:58:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,20 +5,19 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-// DB Connect
 connectDB();
 
-// Middleware
+
 app.use(cors());
 app.use(express.json());
 
-// Static folder for images
+
 app.use("/uploads", express.static("uploads"));
 
-// Routes
+
 app.use("/api/images", require("./routes/imageRoutes"));
 
-// Server Start
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
